75acf4b7d1965b7ffe75ed5491daa759bd4cf9a7
SirLectric43 <domingomiguel4343@gmail.com>
2025-10-31 10:34:57 +0100

    cambio1

--- src/app/List.js
+++ src/app/List.js
@@ -2,7 +2,11 @@ import { people } from './data.js';
 import { getImageUrl } from './utils.js';
 
 export default function List() {
-  const listItems = people.map(person =>
+
+  const chemists = people.filter(person => person.profession === 'químico');
+  const others = people.filter(person => person.profession !== 'químico');
+
+  const chemistList = chemists.map(person =>
     <li key={person.id}>
       <img
         src={getImageUrl(person)}
@@ -15,10 +19,27 @@ export default function List() {
       </p>
     </li>
   );
+
+  const otherList = others.map(person =>
+    <li key={person.id}>
+      <img
+        src={getImageUrl(person)}
+        alt={person.name}
+      />
+      <p>
+        <b>{person.name}:</b>
+        {' ' + person.profession + ' '}
+        conocido/a por {person.accomplishment}
+      </p>
+    </li>
+  );
+
   return (
     <article>
-      <h1>Científicos</h1>
-      <ul>{listItems}</ul>
+      <h1>Químicos</h1>
+      <ul>{chemistList}</ul>
+      <h1>Todos los demás</h1>
+      <ul>{otherList}</ul>
     </article>
   );
 }
